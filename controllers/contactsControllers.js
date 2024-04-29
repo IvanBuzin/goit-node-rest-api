104eeee2102b76183069563834174df998aa9e51
IvanBuzin <ua.buzin@gmail.com>
2024-04-29 09:10:05 +0300

    new

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,61 @@
 import contactsService from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import crypto from "node:crypto";
+import { error } from "node:console";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  const contactsList = await contactsServices.listContacts();
+  try {
+    res.status(200).send(contactsList);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await contactsService.getContactById(id);
+  try {
+    if (contact) {
+      res.status(200).send(contact);
+    } else {
+      res.status(404).send({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await contactsService.removeContact(id);
+  try {
+    if (contact) {
+      res.status(200).send(contact);
+    } else {
+      res.status(404).send({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const { error, value } = createContactSchema.validate({ name, email, phone });
+  console.log(error);
+  if (typeof error !== "undefined") {
+    return res.status(400).send({ message: "Fields must be filled" });
+  }
+  try {
+    const contact = await contactsServices.addContact(name, email, phone);
+    res.status(201).send({ id: crypto.ramdomUUID(), ...value });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const updateContact = (req, res) => {};
